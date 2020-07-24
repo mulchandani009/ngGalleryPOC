b9e3f5e04342490b932332064cdbe3ea7e292c77
dinesh <dineshmulchandani081@gmail.com>
2020-07-24 15:47:46 +0530

    ert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,8 +41,8 @@ export class AppComponent implements OnInit {
 	  {
 	    type: 'video',
 	    src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
-	    thumb: 'https://tubularinsights.com/wp-content/uploads/2015/10/17tips_cover.png',
-	    poster: 'https://tubularinsights.com/wp-content/uploads/2015/10/17tips_cover.png'
+	    thumb: 'https://github.com/mulchandani009/ngGalleryPOC/blob/master/src/assets/images/5.png?raw=true',
+	    poster: 'https://github.com/mulchandani009/ngGalleryPOC/blob/master/src/assets/images/thumbs/5.png?raw=true'
 	  },
 	  {
 	    type: 'youtube',
